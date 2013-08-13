dfd8845e9a3e077d44a142f021b2eb2ac2fbe994
Mayank Mandava <mayankmandava@gmail.com>
2013-08-14 03:54:24 +0530

    removed 'theme' folder, using 'layout' instead

--- app/views/theme/breadcrumb.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="span12">
-	<ul class="breadcrumb">
-		<li><a href="#">Home</a> <span class="divider">/</span></li>
-		<li><a href="#">Account</a> <span class="divider">/</span></li>
-		<li class="active"><a href="#">Register</a></li>
-	</ul>
-</div>
