efe3696caf023f0d6f715266ee144e10f661bf84
Mayank Mandava <mayankmandava@gmail.com>
2013-08-14 04:17:39 +0530

    removed pointless controllers

--- app/views/layout/head.blade.php
+++ app/views/layout/head.blade.php
@@ -5,17 +5,5 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <!-- Le styles -->
-    <!-- {{ HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css')}} -->
-    <!-- {{ HTML::style('/theme/bootstrap/css/bootstrap.min.css')}} -->
-    <!-- {{ HTML::style('/css/boostrap.min.css')}} -->
-    <!-- {{ HTML::style('/css/boostrap.united.min.css')}} -->
     {{ HTML::style('/css/bootstrap.css')}} 
-    <!-- {{ HTML::style('/theme/css/main.css')}} -->
-    <!-- {{ HTML::style('/theme/css/jquery.rating.css')}}  -->
-
-    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-      <![endif]-->
 
