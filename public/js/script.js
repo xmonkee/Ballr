212fc7b539a93ec7d2dc1a728863f3fd52ccaab2
Mayank Mandava <mayankmandava@gmail.com>
2013-08-22 04:41:56 +0530

    bootstrap 3 partially working

--- public/js/script.js
+++
@@ -1,6 +0,0 @@
-$('.image-loader').on('click', function(e) 
-{
-	$('#main-image').attr('href', $(this).attr("href"));
-	$('#main-image > img').attr('src', $(this).attr("href"));
-	return false;
-});
