e4b71064e1e7e0c8a3fdee7e3be48e536355fa4c
Mayank Mandava <mandu@Mayanks-MacBook-Pro.local>
2014-02-07 03:53:38 +0530

    States database seeded

--- app/database/seeds/StatesTableSeeder.php
+++ app/database/seeds/StatesTableSeeder.php
@@ -5,16 +5,36 @@ class StatesTableSeeder extends Seeder {
 	public function run()
 	{
 		// Uncomment the below to wipe the table clean before populating
-		// DB::table('states')->delete();
+		DB::table('states')->delete();
 
 		$states = array(
-		array('state'=>'Odisha'),
-		array('state'=>'Andaman and Nicobar'),
-		array('state'=>'Chandigarh'),
-		array('state'=>'Dadra and Nagar Haveli'),
-		array('state'=>'Daman and Diu'),
-		array('state'=>'Lakshwadeep'),
-		array('state'=>'Puducherry'),
+		array('state'=> 'Andhra Pradesh'),
+		array('state'=> 'Arunachal Pradesh'),
+		array('state'=> 'Assam'),
+		array('state'=> 'Bihar'),
+		array('state'=> 'Chhattisgarh'),
+		array('state'=> 'Goa'),
+		array('state'=> 'Gujarat'),
+		array('state'=> 'Haryana'),
+		array('state'=> 'Himachal Pradesh'),
+		array('state'=> 'Jammu and Kashmir'),
+		array('state'=> 'Jharkhand'),
+		array('state'=> 'Karnataka'),
+		array('state'=> 'Kerala'),
+		array('state'=> 'Madhya Pradesh'),
+		array('state'=> 'Maharashtra'),
+		array('state'=> 'Manipur'),
+		array('state'=> 'Meghalaya'),
+		array('state'=> 'Mizoram'),
+		array('state'=> 'Nagaland'),
+		array('state'=> 'Punjab'),
+		array('state'=> 'Rajasthan'),
+		array('state'=> 'Sikkim'),
+		array('state'=> 'Tamil Nadu'),
+		array('state'=> 'Tripura'),
+		array('state'=> 'Uttar Pradesh'),
+		array('state'=> 'Uttarakhand'),
+		array('state'=> 'West Bengal')
 		);
 
 		// Uncomment the below to run the seeder
--- bootstrap.sh
+++ bootstrap.sh
@@ -90,3 +90,4 @@ echo "CREATE USER 'ballr'@'localhost' IDENTIFIED BY 'thisis3love'" | mysql
 echo "GRANT ALL PRIVILEGES ON ballr.* TO 'ballr'@'localhost' IDENTIFIED BY 'thisis3love'" | mysql
 # Set up the database
 php artisan migrate
+php artisan db:seed
